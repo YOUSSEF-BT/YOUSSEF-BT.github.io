3f77949f9d324d2b9d575e21963865f03f87b086
Youssef Bouzit <bt.youssef.369@gmail.com>
2026-07-30 18:26:07 +0100

    fix: use rebuilt photo as PFE cover

---
+++ src/data/projects/accidentDetectionCover.js
@@ -0,0 +1,6 @@
+import { accidentDetectionProject as baseAccidentDetectionProject } from "./accidentDetection";
+
+export const accidentDetectionProject = {
+  ...baseAccidentDetectionProject,
+  image: "assets/images/projects/accident-detection/main-cover-photo.webp",
+};
