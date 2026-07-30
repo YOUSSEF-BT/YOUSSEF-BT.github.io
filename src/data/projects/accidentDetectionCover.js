1bf9734ebc37e8ecb32b4050ca8a461f3822dffd
Youssef Bouzit <bt.youssef.369@gmail.com>
2026-07-30 18:29:05 +0100

    cleanup: remove unused PFE cover override module

--- src/data/projects/accidentDetectionCover.js
+++
@@ -1,6 +0,0 @@
-import { accidentDetectionProject as baseAccidentDetectionProject } from "./accidentDetection";
-
-export const accidentDetectionProject = {
-  ...baseAccidentDetectionProject,
-  image: "assets/images/projects/accident-detection/main-cover-photo.webp",
-};
